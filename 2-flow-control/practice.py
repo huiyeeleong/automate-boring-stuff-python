d14cab71f1f48885400b8b5f94d447efb57ff802
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-03-21 21:46:38 +1100

    Add Practice Question

---
+++ 2-flow-control/practice.py
@@ -0,0 +1,18 @@
+# spam = 0
+
+# if spam == "1":
+#     print("Hello")
+# elif spam == "2":
+#     print("Howdy")
+# else:
+#     print("Greeting")
+
+# for i in range(1,11):
+#     print(i)
+ 
+ 
+i = 1
+   
+while i < 11:
+    print(i)
+    i += 1
